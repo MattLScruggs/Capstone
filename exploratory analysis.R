2c34fa3268b6d01b1aecc5dbce22d0c5f014e6dc
MattLScruggs <matthewls01@outlook.com>
2025-05-14 16:41:47 -0500

    Initial setup

---
+++ exploratory analysis.R
@@ -0,0 +1,57 @@
+#### setting up libraries and data file locations #### 
+library(tidyverse)
+library(tidytext)
+library(igraph)
+library(ggraph)
+data("stop_words")
+data_loc ='F:/School/Capstone/Data sets/cord-19_2020-04-10/2020-04-10'
+
+
+#### reading in the data ####
+meta_data<-read_csv(paste(data_loc,"/metadata.csv"))
+
+colnames(meta_data)
+
+abs_df<-meta_data[,c('cord_uid','abstract')]%>%
+  unnest_tokens(bigram, abstract, token = 'ngrams', n=2)%>%
+  filter(!is.na(bigram))
+
+
+bigram_sep<-abs_df%>%
+  separate(bigram, c("word1", "word2"), sep = " ")%>%
+  filter(!word1 %in% stop_words$word) %>%
+  filter(!word2 %in% stop_words$word)
+
+bigram_counts<-bigram_sep%>%
+  count(word1, word2, sort =T)
+
+
+
+bigram_graph<-bigram_counts%>%
+  filter(n>=50)%>%
+  graph_from_data_frame()
+
+bg_graph<-ggraph(bigram_graph, layout = "fr") +
+  geom_edge_link(aes(edge_alpha = n), show.legend = FALSE, end_cap = circle(.07, 'inches')) +
+  geom_node_point(color = "lightblue", size = 5) +
+  geom_node_text(aes(label = name), vjust = 1, hjust = 1) +
+  theme_void()
+
+filtered_graph<-bigram_graph%>%delete_vertices(V(bigram_graph)[degree(bigram_graph)<10])
+
+ggraph(filtered_graph, layout = "fr") +
+  geom_edge_link(aes(edge_alpha = n), show.legend = FALSE, end_cap = circle(.07, 'inches')) +
+  geom_node_point(color = "lightblue", size = 5) +
+  geom_node_text(aes(label = name), vjust = 1, hjust = 1) +
+  theme_void()
+
+
+R0_graph<-bigram_counts%>%
+  filter(word1 == 'transmission' | word2 == 'transmission')%>%
+  graph_from_data_frame()
+
+ggraph(R0_graph, layout = "fr") +
+  geom_edge_link(aes(edge_alpha = n), show.legend = FALSE, end_cap = circle(.07, 'inches')) +
+  geom_node_point(color = "lightblue", size = 5) +
+  geom_node_text(aes(label = name), vjust = 1, hjust = 1) +
+  theme_void()
